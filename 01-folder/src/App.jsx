2ee0676b44c9bbe3ad639fceff945f8d3144ebd5
Sudeep Yadav <sudeep@gmail.com>
2026-05-11 22:35:25 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>My name is sudeep yadav</h1>
+      <h3> my age is 21</h3>
+    </div>
+  )
+}
+
+export default App
